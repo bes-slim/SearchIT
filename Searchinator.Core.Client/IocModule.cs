1ab5f566a8b2b4bc5ce77dccce24c5fdd8d534b1
Bessem Slim <slim.bessem@gmail.com>
2014-12-16 10:36:04 +0100

    add ioc to client

---
+++ Searchinator.Core.Client/IocModule.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Ninject;
+using Ninject.Modules;
+using Searchinator.Core.Contracts;
+
+namespace Searchinator.Core.Client
+{
+    public class IocModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IBaseSearchRule>().To<GeneralInfoSearch>();
+            Bind<IBaseSearchRule>().To<TextSearch>();
+
+
+            Bind<IGeneralInfoSearchRule>().To<PriceToRule>();
+            Bind<IGeneralInfoSearchRule>().To<PriceFromRule>();
+
+
+            Bind<ISearch>()
+            .To<Search>()
+            .WithConstructorArgument(
+                Search.CTOR_SEARCH_RULES_ARG_NAME,
+                x => x.Kernel.GetAll<IBaseSearchRule>().ToList());
+
+
+            Bind<IGeneralInfoSectionSearch>()
+            .To<GeneralInfoSectionSearch>()
+            .WithConstructorArgument(
+                GeneralInfoSectionSearch.CTOR_GENERAL_INFO_SEARCH_RULES_ARG_NAME,
+                x => x.Kernel.GetAll<IGeneralInfoSearchRule>().ToList());
+        }
+    }
+}
--- Searchinator.Core.Client/Program.cs
+++ Searchinator.Core.Client/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ninject;
+using Searchinator.Core.Contracts;
+using Searchinator.Models;
 
 namespace Searchinator.Core.Client
 {
@@ -10,6 +13,22 @@ namespace Searchinator.Core.Client
     {
         static void Main(string[] args)
         {
+            IKernel kernel = new StandardKernel(new IocModule());
+            var search = kernel.Get<ISearch>();
+            var query = new SearchQuery()
+            {
+                Text = "my keyword",
+                GeneralInfo = new GeneralInfo()
+                {
+                    PriceFrom = 10000
+                    //PriceTo = 50000
+                }
+            };
+            Console.WriteLine(search.SearchFor(query));
+            Console.ReadKey();
+
+
+
         }
     }
 }
